43b66efc00912f94b3b2cc88fa3cf8a45b8a5401
Yuri Young <yuri.young@qq.com>
2020-05-11 02:09:06 +0800

    can instanciable Playlist in QML

--- src/declarative/QmlMediaPlaylist.cpp
+++ src/declarative/QmlMediaPlaylist.cpp
@@ -1,49 +1,102 @@
-#include "QmlPlaylistModel.h"
+#include "QmlMediaPlaylist.h"
 
+#include <QQmlListProperty>
 #include <QFileInfo>
 
-QmlPlaylistModel::QmlPlaylistModel(QObject *parent)
-    : QAbstractItemModel(parent)
+QmlMediaItem::QmlMediaItem(QObject *parent) : QObject(parent)
 {
-    setPlaylist(new MediaPlaylist());
+
 }
 
-int QmlPlaylistModel::rowCount(const QModelIndex &parent) const
+QUrl QmlMediaItem::source() const
+{
+    return m_source;
+}
+
+void QmlMediaItem::setSource(QUrl source)
+{
+    m_source = source;
+}
+
+/**
+ * @brief QmlMediaPlaylist::QmlMediaPlaylist
+ * @param parent
+ */
+QmlMediaPlaylist::QmlMediaPlaylist(QObject *parent)
+    : QAbstractListModel(parent)
+    , m_error(MediaPlaylist::NoError)
+    , m_readOnly(false)
+{
+
+}
+
+QmlMediaPlaylist::~QmlMediaPlaylist()
+{
+
+}
+
+void QmlMediaPlaylist::classBegin()
+{
+    m_playlist.reset(new MediaPlaylist());
+
+    // connect signals-signals
+    connect(m_playlist.data(), &MediaPlaylist::currentIndexChanged, this, &QmlMediaPlaylist::currentIndexChanged);
+    connect(m_playlist.data(), &MediaPlaylist::playbackModeChanged, this, &QmlMediaPlaylist::playbackModeChanged);
+    connect(m_playlist.data(), &MediaPlaylist::currentMediaChanged, this, &QmlMediaPlaylist::currentSourceChanged);
+    connect(m_playlist.data(), &MediaPlaylist::loadSucceed, this, &QmlMediaPlaylist::loadSucceed);
+
+    // connect signals-slots
+    connect(m_playlist.data(), &MediaPlaylist::mediaAboutToInserted, this, &QmlMediaPlaylist::beginInsertItems);
+    connect(m_playlist.data(), &MediaPlaylist::mediaInserted, this, &QmlMediaPlaylist::endInsertItems);
+    connect(m_playlist.data(), &MediaPlaylist::mediaAboutToRemoved, this, &QmlMediaPlaylist::beginRemoveItems);
+    connect(m_playlist.data(), &MediaPlaylist::mediaRemoved, this, &QmlMediaPlaylist::endRemoveItems);
+    connect(m_playlist.data(), &MediaPlaylist::mediaChanged, this, &QmlMediaPlaylist::changeItems);
+    connect(m_playlist.data(), &MediaPlaylist::mediaChanged, this, &QmlMediaPlaylist::changeItems);
+    connect(m_playlist.data(), &MediaPlaylist::loadFailed, this, &QmlMediaPlaylist::playlistLoadFailed);
+
+    if(m_playlist->isReadOnly())
+    {
+        m_readOnly = true;
+        emit readOnlyChanged();
+    }
+}
+
+void QmlMediaPlaylist::componentComplete()
+{
+    // do nothing
+}
+
+int QmlMediaPlaylist::rowCount(const QModelIndex &parent) const
 {
     return m_playlist && !parent.isValid() ? m_playlist->mediaCount() : 0;
 }
 
-int QmlPlaylistModel::columnCount(const QModelIndex &parent) const
+int QmlMediaPlaylist::columnCount(const QModelIndex &parent) const
 {
-    return !parent.isValid() ? Columns : 0;
+    return !parent.isValid() ? ColumnsRole : 0;
 }
 
-QModelIndex QmlPlaylistModel::index(int row, int column, const QModelIndex &parent) const
+QModelIndex QmlMediaPlaylist::index(int row, int column, const QModelIndex &parent) const
 {
     if(!m_playlist || parent.isValid())
         return QModelIndex();
 
-    if(row >= 0 && row < m_playlist->mediaCount() && column >= 0 && column < Columns)
+    if(row >= 0 && row < m_playlist->mediaCount() && column >= 0 && column < ColumnsRole)
         return createIndex(row, column);
 
     return QModelIndex();
 }
 
-QModelIndex QmlPlaylistModel::parent(const QModelIndex &child) const
+QVariant QmlMediaPlaylist::data(const QModelIndex &index, int role) const
 {
-    Q_UNUSED(child)
-    return QModelIndex();
-}
-
-QVariant QmlPlaylistModel::data(const QModelIndex &index, int role) const
-{
-    if (index.isValid() && role == Qt::DisplayRole)
+    Q_UNUSED(role)
+    if (index.isValid() /*&& role == Qt::DisplayRole*/)
     {
         QVariant value = m_data[index];
-        if (!value.isValid() && index.column() == Title)
+        if (!value.isValid())
         {
             QUrl location = m_playlist->media(index.row()).canonicalUrl();
-            return QFileInfo(location.path()).fileName();
+            return role == TitleRole ? QFileInfo(location.path()).fileName() : location;
         }
 
         return value;
@@ -51,7 +104,7 @@ QVariant QmlPlaylistModel::data(const QModelIndex &index, int role) const
     return QVariant();
 }
 
-bool QmlPlaylistModel::setData(const QModelIndex &index, const QVariant &value, int role)
+bool QmlMediaPlaylist::setData(const QModelIndex &index, const QVariant &value, int role)
 {
     Q_UNUSED(role)
     m_data[index] = value;
@@ -59,82 +112,262 @@ bool QmlPlaylistModel::setData(const QModelIndex &index, const QVariant &value, 
     return true;
 }
 
-MediaPlaylist *QmlPlaylistModel::playlist() const
+QHash<int, QByteArray> QmlMediaPlaylist::roleNames() const
 {
-    return m_playlist.data();
+    QHash<int, QByteArray> roleNames;
+    roleNames[TitleRole]    = "title";
+    roleNames[ArtistRole]   = "artist";
+    roleNames[DurationRole] = "duration";
+    roleNames[SourceRole]   = "source";
+    return roleNames;
 }
 
-void QmlPlaylistModel::setPlaylist(MediaPlaylist *playlist)
+QmlMediaPlaylist::PlaybackMode QmlMediaPlaylist::playbackMode() const
 {
-    if (m_playlist)
+    return PlaybackMode(m_playlist->playbackMode());
+}
+
+void QmlMediaPlaylist::setPlaybackMode(QmlMediaPlaylist::PlaybackMode mode)
+{
+    if(playbackMode() == mode)
+        return;
+
+    m_playlist->setPlaybackMode(MediaPlaylist::PlaybackMode(mode));
+}
+
+int QmlMediaPlaylist::currentIndex() const
+{
+    return m_playlist->currentIndex();
+}
+
+void QmlMediaPlaylist::setCurrentIndex(int index)
+{
+    if(currentIndex() == index)
+        return;
+
+    m_playlist->setCurrentIndex(index);
+}
+
+MediaPlaylist *QmlMediaPlaylist::mediaPlaylist() const
+{
+    return m_playlist.get();
+}
+
+int QmlMediaPlaylist::itemCount() const
+{
+    return m_playlist->mediaCount();
+}
+
+bool QmlMediaPlaylist::readOnly() const
+{
+    return m_readOnly;
+}
+
+QmlMediaPlaylist::Error QmlMediaPlaylist::error() const
+{
+    return Error(m_error);
+}
+
+QString QmlMediaPlaylist::errorString() const
+{
+    return m_errorString;
+}
+
+QUrl QmlMediaPlaylist::currentSource() const
+{
+    return m_playlist->currentMedia().canonicalUrl();
+}
+
+QQmlListProperty<QmlMediaItem> QmlMediaPlaylist::items()
+{
+    return QQmlListProperty<QmlMediaItem>(this, this,
+        &QmlMediaPlaylist::item_append,
+        &QmlMediaPlaylist::item_count,
+        nullptr, // item at
+        &QmlMediaPlaylist::item_clear
+    );
+}
+
+QUrl QmlMediaPlaylist::source(int index)
+{
+    return m_playlist->media(index).canonicalUrl();
+}
+
+int QmlMediaPlaylist::nextIndex(int steps)
+{
+    return m_playlist->nextIndex(steps);
+}
+
+int QmlMediaPlaylist::previousIndex(int steps)
+{
+    return m_playlist->previousIndex(steps);
+}
+
+void QmlMediaPlaylist::next()
+{
+    m_playlist->next();
+}
+
+void QmlMediaPlaylist::previous()
+{
+    m_playlist->previous();
+}
+
+void QmlMediaPlaylist::shuffle()
+{
+    m_playlist->shuffle();
+}
+
+void QmlMediaPlaylist::load(const QUrl &location, const QString &format)
+{
+    m_error = MediaPlaylist::NoError;
+    m_errorString = QString();
+    emit errorChanged();
+    m_playlist->load(location, format.toLatin1().constData());
+}
+
+bool QmlMediaPlaylist::save(const QUrl &location, const QString &format)
+{
+    return m_playlist->save(location, format.toLatin1().constData());
+}
+
+bool QmlMediaPlaylist::addItem(const QUrl &source)
+{
+    return m_playlist->addMedia(Media(source));
+}
+
+bool QmlMediaPlaylist::addItems(const QList<QUrl> &sources)
+{
+    if(sources.isEmpty())
+        return false;
+
+    QList<Media> contents;
+    QList<QUrl>::const_iterator it = sources.constBegin();
+    while (it != sources.constEnd())
     {
-        disconnect(m_playlist.data(), &MediaPlaylist::mediaAboutToInserted, this, &QmlPlaylistModel::beginInsertItems);
-        disconnect(m_playlist.data(), &MediaPlaylist::mediaInserted, this, &QmlPlaylistModel::endInsertItems);
-        disconnect(m_playlist.data(), &MediaPlaylist::mediaAboutToRemoved, this, &QmlPlaylistModel::beginRemoveItems);
-        disconnect(m_playlist.data(), &MediaPlaylist::mediaRemoved, this, &QmlPlaylistModel::endRemoveItems);
-        disconnect(m_playlist.data(), &MediaPlaylist::mediaChanged, this, &QmlPlaylistModel::changeItems);
+        contents.push_back(Media(*it));
+        ++it;
     }
 
-    beginResetModel();
-    m_playlist.reset(playlist);
-    if (m_playlist)
-    {
-        connect(m_playlist.data(), &MediaPlaylist::mediaAboutToInserted, this, &QmlPlaylistModel::beginInsertItems);
-        connect(m_playlist.data(), &MediaPlaylist::mediaInserted, this, &QmlPlaylistModel::endInsertItems);
-        connect(m_playlist.data(), &MediaPlaylist::mediaAboutToRemoved, this, &QmlPlaylistModel::beginRemoveItems);
-        connect(m_playlist.data(), &MediaPlaylist::mediaRemoved, this, &QmlPlaylistModel::endRemoveItems);
-        connect(m_playlist.data(), &MediaPlaylist::mediaChanged, this, &QmlPlaylistModel::changeItems);
-    }
-    endResetModel();
+    return m_playlist->addMedia(contents);
 }
 
-void QmlPlaylistModel::add(const QList<QUrl> &urls)
+bool QmlMediaPlaylist::insertItem(int index, const QUrl &source)
 {
-    foreach (auto url, urls)
-    {
-        m_playlist->addMedia(url);
-    }
+    return m_playlist->insertMedia(index, Media(source));
 }
 
-void QmlPlaylistModel::remove(int index)
+bool QmlMediaPlaylist::insertItems(int index, const QList<QUrl> &sources)
 {
-    m_playlist->removeMedia(index);
+    if (sources.empty())
+            return false;
+
+    QList<Media> contents;
+    QList<QUrl>::const_iterator it = sources.constBegin();
+    while (it != sources.constEnd())
+    {
+        contents.push_back(Media(*it));
+        ++it;
+    }
+
+    return m_playlist->insertMedia(index, contents);
 }
 
-void QmlPlaylistModel::clear()
+bool QmlMediaPlaylist::moveItem(int from, int to)
+{
+    return m_playlist->moveMedia(from, to);
+}
+
+bool QmlMediaPlaylist::removeItem(int index)
+{
+    return m_playlist->removeMedia(index);
+}
+
+bool QmlMediaPlaylist::removeItems(int start, int end)
+{
+    return m_playlist->removeMedia(start, end);
+}
+
+bool QmlMediaPlaylist::clear()
 {
     beginResetModel();
 
     m_data.clear();
-    m_playlist->clear();
+    bool ok = m_playlist->clear();
 
     endResetModel();
+
+    return ok;
 }
 
-void QmlPlaylistModel::beginInsertItems(int start, int end)
+void QmlMediaPlaylist::beginInsertItems(int start, int end)
 {
+    emit itemAboutInsert(start, end);
+
     m_data.clear();
     beginInsertRows(QModelIndex(), start, end);
 }
 
-void QmlPlaylistModel::endInsertItems()
+void QmlMediaPlaylist::endInsertItems(int start, int end)
 {
     endInsertRows();
+
+    emit itemCountChanged();
+    emit itemInserted(start, end);
 }
 
-void QmlPlaylistModel::beginRemoveItems(int start, int end)
+void QmlMediaPlaylist::beginRemoveItems(int start, int end)
 {
+    emit itemAboutRemove(start, end);
+
     m_data.clear();
     beginRemoveRows(QModelIndex(), start, end);
 }
 
-void QmlPlaylistModel::endRemoveItems()
+void QmlMediaPlaylist::endRemoveItems(int start, int end)
 {
     endInsertRows();
+
+    emit itemCountChanged();
+    emit itemRemoved(start, end);
 }
 
-void QmlPlaylistModel::changeItems(int start, int end)
+void QmlMediaPlaylist::changeItems(int start, int end)
 {
     m_data.clear();
-    emit dataChanged(index(start,0), index(end, Columns));
+    emit dataChanged(index(start,0), index(end, ColumnsRole));
+    emit itemChanged(start, end);
+}
+
+void QmlMediaPlaylist::playlistLoadFailed()
+{
+    m_error = m_playlist->error();
+    m_errorString = m_playlist->errorString();
+
+    emit errorChanged();
+    emit loadFailed();
+}
+
+void QmlMediaPlaylist::item_append(QQmlListProperty<QmlMediaItem> *list, QmlMediaItem *item)
+{
+    static_cast<QmlMediaPlaylist*>(list->object)->addItem(item->source());
+}
+
+int QmlMediaPlaylist::item_count(QQmlListProperty<QmlMediaItem> *list)
+{
+    return static_cast<QmlMediaPlaylist*>(list->object)->itemCount();
+}
+
+/**
+ * @brief unused
+ * @return
+ */
+QmlMediaItem *QmlMediaPlaylist::item_at(QQmlListProperty<QmlMediaItem> *, int)
+{
+    return nullptr;
+}
+
+void QmlMediaPlaylist::item_clear(QQmlListProperty<QmlMediaItem> *list)
+{
+    static_cast<QmlMediaPlaylist*>(list->object)->clear();
 }
--- src/declarative/QmlMediaPlaylist.h
+++ src/declarative/QmlMediaPlaylist.h
@@ -1,52 +1,163 @@
-#ifndef QMLPLAYLISTMODEL_H
-#define QMLPLAYLISTMODEL_H
+#ifndef QMLMEDIAPLAYLIST_H
+#define QMLMEDIAPLAYLIST_H
 
 #include "player/MediaPlaylist.h"
-#include <QAbstractItemModel>
+#include <QAbstractListModel>
+#include <QtQml>
 
-class QmlPlaylistModel : public QAbstractItemModel
+class QmlMediaItem : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QUrl source READ source WRITE setSource)
 public:
-    enum Column
-    {
-        Title = 0,
-        Artist,
-        Duration,
+    QmlMediaItem(QObject *parent = nullptr);
 
-        Columns,
+    QUrl source() const;
+    void setSource(QUrl source);
+
+private:
+    QUrl m_source;
+};
+
+class QmlMediaPlaylist : public QAbstractListModel, public QQmlParserStatus
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(QmlMediaPlaylist)
+    Q_PROPERTY(PlaybackMode playbackMode READ playbackMode WRITE setPlaybackMode NOTIFY playbackModeChanged)
+    Q_PROPERTY(int currentIndex READ currentIndex WRITE setCurrentIndex NOTIFY currentIndexChanged)
+    Q_PROPERTY(QUrl currentSource READ currentSource NOTIFY currentSourceChanged)
+    Q_PROPERTY(int itemCount READ itemCount NOTIFY itemCountChanged)
+    Q_PROPERTY(bool readOnly READ readOnly NOTIFY readOnlyChanged)
+    Q_PROPERTY(Error error READ error NOTIFY errorChanged)
+    Q_PROPERTY(QString errorString READ errorString NOTIFY errorChanged)
+    Q_PROPERTY(QQmlListProperty<QmlMediaItem> items READ items)
+    Q_ENUMS(PlaybackMode Error)
+    Q_INTERFACES(QQmlParserStatus)
+    Q_CLASSINFO("DefaultProperty", "items")
+public:
+    enum Roles
+    {
+        TitleRole = Qt::UserRole + 1,
+        ArtistRole,
+        DurationRole,
+        SourceRole,
+
+        ColumnsRole,
     };
 
-    explicit QmlPlaylistModel(QObject *parent = nullptr);
+    enum PlaybackMode
+    {
+        CurrentItemOnce = MediaPlaylist::CurrentItemOnce,
+        CurrentItemRepeat = MediaPlaylist::CurrentItemRepeat,
+        Sequential = MediaPlaylist::Sequential,
+        Loop = MediaPlaylist::Loop,
+        Random = MediaPlaylist::Random
+    };
+
+    enum Error
+    {
+        NoError = MediaPlaylist::NoError,
+        FormatError = MediaPlaylist::FormatError,
+        FormatNotSupportedError = MediaPlaylist::FormatNotSupportedError,
+        NetworkError = MediaPlaylist::NetworkError,
+        AccessDeniedError = MediaPlaylist::AccessDeniedError
+    };
+
+    explicit QmlMediaPlaylist(QObject *parent = nullptr);
+    ~QmlMediaPlaylist() override;
+
+    void classBegin() override;
+    void componentComplete() override;
 
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
 
     QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const override;
-    QModelIndex parent(const QModelIndex &child) const override;
 
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
     bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::DisplayRole) override;
 
-    MediaPlaylist *playlist() const;
-    void setPlaylist(MediaPlaylist *playlist);
+    QHash<int, QByteArray> roleNames() const override;
 
-    Q_INVOKABLE void add(const QList<QUrl> &urls);
-    Q_INVOKABLE void remove(int index);
-    Q_INVOKABLE void clear();
+    PlaybackMode playbackMode() const;
+    void setPlaybackMode(PlaybackMode mode);
+
+    int currentIndex() const;
+    void setCurrentIndex(int index);
+
+    MediaPlaylist *mediaPlaylist() const;
+
+    int itemCount() const;
+    bool readOnly() const;
+    Error error() const;
+    QString errorString() const;
+    QUrl currentSource() const;
+
+    QQmlListProperty<QmlMediaItem> items();
+
+signals:
+    void currentIndexChanged();
+    void playbackModeChanged();
+    void currentSourceChanged();
+    void readOnlyChanged();
+    void itemCountChanged();
+    void errorChanged();
+    void loadSucceed();
+    void loadFailed();
+
+    void itemAboutInsert(int start, int end);
+    void itemInserted(int start, int end);
+    void itemAboutRemove(int start, int end);
+    void itemRemoved(int start, int end);
+    void itemChanged(int start, int end);
 
 public slots:
+    QUrl source(int index);
+
+    int nextIndex(int steps = 1);
+    int previousIndex(int steps = 1);
+    void next();
+    void previous();
+    void shuffle();
+
+    void load(const QUrl &location, const QString &format = QString());
+    bool save(const QUrl &location, const QString &format = QString());
+
+    bool addItem(const QUrl &source);
+    bool addItems(const QList<QUrl> &sources);
+
+    bool insertItem(int index, const QUrl &source);
+    bool insertItems(int index, const QList<QUrl> &sources);
+
+    bool moveItem(int from, int to);
+    bool removeItem(int index);
+    bool removeItems(int start, int end);
+
+    bool clear();
 
 private slots:
     void beginInsertItems(int start, int end);
-    void endInsertItems();
+    void endInsertItems(int start, int end);
     void beginRemoveItems(int start, int end);
-    void endRemoveItems();
+    void endRemoveItems(int start, int end);
     void changeItems(int start, int end);
+    void playlistLoadFailed();
+
+private:
+    static void item_append(QQmlListProperty<QmlMediaItem> *list, QmlMediaItem* item);
+    static int item_count(QQmlListProperty<QmlMediaItem> *list);
+    static QmlMediaItem *item_at(QQmlListProperty<QmlMediaItem> *, int);
+    static void item_clear(QQmlListProperty<QmlMediaItem> *list);
 
 private:
     QScopedPointer<MediaPlaylist> m_playlist;
     QMap<QModelIndex, QVariant> m_data;
+    QString m_errorString;
+    MediaPlaylist::Error m_error;
+    bool m_readOnly;
 };
 
-#endif // QMLPLAYLISTMODEL_H
+QML_DECLARE_TYPE(QT_PREPEND_NAMESPACE(QmlMediaItem))
+QML_DECLARE_TYPE(QT_PREPEND_NAMESPACE(QmlMediaPlaylist))
+
+#endif // QMLMEDIAPLAYLIST_H
